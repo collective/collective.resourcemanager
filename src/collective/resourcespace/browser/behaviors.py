8554e3a45f64876270d5e49facbd83f25170c80a
Chrissy Wainwright <chrissy@sixfeetup.com>
2019-12-03 14:26:08 -0600

    allow for image URL that will save as a NamedBlobImage

--- src/collective/resourcespace/browser/behaviors.py
+++ src/collective/resourcespace/browser/behaviors.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+import requests
 from plone.autoform import directives
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
 from plone.namedfile import field as namedfile
+from plone.namedfile.file import NamedBlobImage
 from plone.supermodel import model
-from zope.interface import alsoProvides
+from zope.component import adapter
+from zope.interface import provider, implementer
 
 from collective.resourcespace.browser.widget import NamedRSImageWidget
 
 
+@provider(IFormFieldProvider)
 class IBrowseRS(model.Schema):
 
     image = namedfile.NamedBlobImage(
@@ -20,4 +25,32 @@ class IBrowseRS(model.Schema):
         NamedRSImageWidget)
 
 
-alsoProvides(IBrowseRS, IFormFieldProvider)
+@implementer(IBrowseRS)
+@adapter(IDexterityContent)
+class BrowseRS(object):
+    """If URL was entered, store to NamedBlobImage
+    """
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def image(self):
+        return self.context.image
+
+    @image.setter
+    def image(self, value):
+        req = self.context.REQUEST
+        url = 'rs-url-input' in req and req['rs-url-input']
+        if url:
+            response = requests.get(url)
+            # TODO: handle errors on url
+            blob = NamedBlobImage(
+                data=response.content)
+            curr_img = self.context.image
+            if not curr_img:
+                self.context.image = blob
+            elif self.context.image.getFirstBytes() != blob.getFirstBytes():
+                self.context.image = blob
+        else:
+            self.context.image = value
