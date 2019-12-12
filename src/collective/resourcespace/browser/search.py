c2a4612f5fcbe13b757343ee7346642c0edeb4be
Chrissy Wainwright <chrissy@sixfeetup.com>
2019-12-12 14:34:50 -0600

    add the user parameter in the query function
    This will allow the function to be used in other places

--- src/collective/resourcespace/browser/search.py
+++ src/collective/resourcespace/browser/search.py
@@ -2,7 +2,6 @@ import hashlib
 import json
 import requests
 import urllib.parse
-from plone import api
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -26,9 +25,10 @@ class ResourceSpaceSearch(BrowserView):
 
     def query_resourcespace(self, query):
         hash = hashlib.sha256()
-        key_query = self.rs_private_key + query
+        user_query = 'user={0}'.format(self.rs_user) + query
+        key_query = self.rs_private_key + user_query
         hash.update(key_query.encode('utf-8'))
-        request_url = self.rs_url + '?' + query + '&sign=' + hash.hexdigest()
+        request_url = self.rs_url + '?' + user_query + '&sign=' + hash.hexdigest()
         response = requests.get(request_url)
         if response.status_code != 200:
             self.messages.append(response.reason)
@@ -46,16 +46,16 @@ class ResourceSpaceSearch(BrowserView):
             search_term = urllib.parse.quote_plus(form['rs_search'])
         else:
             search_term = urllib.parse.quote_plus('!' + browse_term)
-        query = 'user={0}&function=do_search&param1={1}&param2=1'.format(
-            self.rs_user, search_term
+        query = '&function=do_search&param1={1}&param2=1'.format(
+            search_term
         )
         response = self.query_resourcespace(query)
         self.image_metadata = {x['ref']: x for x in response}
         self.num_results = len(response)
         media_ids = [x['ref'] for x in response[:100]]
         # build new query to return image urls
-        query2 = 'user={0}&function=get_resource_path&param1=%5B{1}%5D&param2=false&param3=scr'.format(
-            self.rs_user, ','.join(media_ids)
+        query2 = '&function=get_resource_path&param1=%5B{1}%5D&param2=false&param3=scr'.format(
+            ','.join(media_ids)
         )
         self.image_urls = self.query_resourcespace(query2)
         if not self.image_urls and not self.messages:
@@ -68,8 +68,6 @@ class ResourceSpaceSearch(BrowserView):
         return self.template()
 
     def collections(self):
-        query = 'user={0}&function=search_public_collections&param2=name&param3=ASC&param4=0'.format(
-            self.rs_user
-        )
+        query = '&function=search_public_collections&param2=name&param3=ASC&param4=0'
         response = self.query_resourcespace(query)
         return response
